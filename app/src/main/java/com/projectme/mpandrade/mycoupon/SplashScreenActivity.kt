122264522739a65bbdaaddab8651fd360ee61e1e
cin_mandrade <cin_mandrade@uolinc.com>
2019-01-23 15:00:23 -0200

    SplashCreen no app

---
+++ app/src/main/java/com/projectme/mpandrade/mycoupon/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.projectme.mpandrade.mycoupon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val handle = Handler()
+
+        handle.postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 2000L)
+    }
+}
